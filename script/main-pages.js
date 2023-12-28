5d91bcd0a2f70a070ea3fb3f8cccd50b82e76b73
Oscar <szkrtests@gmail.com>
2023-12-28 17:15:50 +0100

    6.1

--- script/main-pages.js
+++ script/main-pages.js
@@ -2,28 +2,7 @@
 
 // var storedCoordinates, storedZoom;
 
-google.addTo(map);
 
-map.on('moveend', function() {
-    var currentCoordinates = map.getCenter();
-    var currentZoom = map.getZoom();
-    localStorage.setItem('mapCoordinates', JSON.stringify(currentCoordinates));
-    localStorage.setItem('mapZoom', currentZoom.toString());
-});
-
-var currentLayer = google;
-
-function switchLayer() {
-    if (currentLayer === google) {
-        map.removeLayer(google);
-        apple.addTo(map);
-        currentLayer = apple;
-    } else {
-        map.removeLayer(apple);
-        google.addTo(map);
-        currentLayer = google;
-    }
-};
 
 
 
