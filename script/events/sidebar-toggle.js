c4d693818edb5c158bfe1d1d5e2ed0abc490dcd1
Oscar <szkrtests@gmail.com>
2023-12-30 20:49:09 +0100

    Build 6.853

--- script/events/sidebar-toggle.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    function toggleSidebar() {
-        const sidebar = $('#event-sidebox');
-        sidebar.toggleClass('hidden');
-    }
-});
--- script/events/sidebox-time-show.js
+++
@@ -1,9 +0,0 @@
-Handlebars.registerHelper('formatTime', function (time) {
-    const formattedTime = new Date(time).toLocaleTimeString('pl-PL', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-
-    });
-    return formattedTime;
-});
