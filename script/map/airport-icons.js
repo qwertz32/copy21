d88994f9b9f0420555d3c50dfc09381122c8f54c
Oscar <szkrtests@gmail.com>
2024-01-15 22:35:54 +0100

    Removed comments and simplified the code

--- script/map/airport-icons.js
+++ script/map/airport-icons.js
@@ -7,63 +7,34 @@ $(document).ready(() => {
         popupAnchor: [-3, -15],
         className: 'airport-icon-transition',
     });
-
     const markersLayer = L.layerGroup().addTo(map);
-    let airportData;
-    let airportsVisible = true;
-    let lsAirports = localStorage.getItem('airports');
+    let airportData, airportsVisible = true, lsAirports = localStorage.getItem('airports');
 
-    function addAirportMarker(airport) {
+    function addMarker(airport) {
         if (airport.icao && airport.lat && airport.lon) {
-            const marker = L.marker([airport.lat, airport.lon], {
-                icon: airportIcon,
-            });
-    
-            // Create a popup content string
-            const popupContent = airport.name + ' ' + airport.icao || 'Unnamed Airport';
-    
-            // Bind the popup with the correct class name and set the title
-            marker.bindPopup(popupContent, {
+            const marker = L.marker([airport.lat, airport.lon], { icon: airportIcon });
+            marker.bindPopup(airport.name + ' ' + airport.icao || 'Unnamed Airport', {
                 className: 'airport-custom-popup',
-                closeButton: false, // Optional: to hide the close button
-            }).openPopup(); // Open the popup to set the title
-    
-            // Set the title directly in JavaScript
-            const popup = marker.getPopup();
-            if (popup) {
-                popup.options.title = 'helo';
-            }
-    
+                closeButton: false,
+            }).openPopup();
             markersLayer.addLayer(marker);
         }
     }
-    
-    
-    
 
-    // Function to fetch airports data
-    function fetchAirportsData() {
-        const airportsJson = '/src/json/airports-2.json';
-        return fetch(airportsJson)
-            .then((response) => response.json())
-            .then((data) => {
-                airportData = data.rows;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+    async function fetchAirportsData() {
+        return fetch('/src/json/airports-2.json')
+            .then(response => response.json())
+            .then(data => airportData = data.rows)
+            .catch(error => console.error(error));
     }
 
-    // Function to get airports based on visibility and cached data
     function getAirports() {
         const bounds = map.getBounds();
 
         if (lsAirports === 'visible') {
             if (!airportData) {
-                // Fetch data only if not already cached
                 fetchAirportsData().then(() => displayAirports(bounds));
             } else {
-                // Use cached data
                 displayAirports(bounds);
             }
         } else if (airportsVisible) {
@@ -73,63 +44,47 @@ $(document).ready(() => {
                 displayAirports(bounds);
             }
         } else {
-            // Hide airports if not visible
             markersLayer.clearLayers();
         }
     }
 
-    // Function to display airports on the map
     function displayAirports(bounds) {
         markersLayer.clearLayers();
         let addedAirports = 0;
 
-        airportData.forEach((airport) => {
+        airportData.forEach(airport => {
             if (isMarkerVisible(airport, bounds) && addedAirports < 350) {
-                addAirportMarker(airport);
+                addMarker(airport);
                 addedAirports++;
             }
         });
     }
+
     function isMarkerVisible(airport, bounds) {
         const markerLatLng = L.latLng(airport.lat, airport.lon);
         return bounds.contains(markerLatLng);
     }
-    // Function to check if airports are supposed to be shown
+
     function checkIfAirportsShown() {
         airportsVisible = lsAirports !== 'hidden';
         updateAirportVisibility();
     }
 
-    // Function to update airports visibility
     function updateAirportVisibility() {
-        if (airportsVisible) {
-            getAirports();
-        } else {
-            markersLayer.clearLayers();
-        }
+        airportsVisible ? getAirports() : markersLayer.clearLayers();
     }
 
-    // Initial check for airports visibility
     checkIfAirportsShown();
 
-    // Function to handle airport toggle button click
     airportToggleBtn.click(function () {
         airportsVisible = !airportsVisible;
-        if (airportsVisible) {
-            localStorage.setItem('airports', 'visible');
-        } else {
-            localStorage.setItem('airports', 'hidden');
-        }
+        localStorage.setItem('airports', airportsVisible ? 'visible' : 'hidden');
         updateAirportVisibility();
     });
 
-    // Event listener for map movement/zoom changes
     map.on('moveend zoomend', function () {
-        if (airportsVisible) {
-            getAirports();
-        }
+        airportsVisible && getAirports();
     });
 
-    // Initial fetch and display of airports data
     getAirports();
 });
