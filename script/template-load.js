9ea1b63412956068e937fc0ec09dc5cd7af26474
Oscar <szkrtests@gmail.com>
2024-01-16 22:38:27 +0100

    Added separate HTML loading into the main

---
+++ script/template-load.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $('#flight-details').load('/templates/map/plane-info.html'); //Plane info, MAP subpage
+    $('#ls-popup').load('/templates/popups/first.html');
+});
