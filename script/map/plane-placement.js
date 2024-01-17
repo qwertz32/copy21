e63fc6f0dbc10a92aaa3b402c441b5cb7d2823ba
Oscar <szkrtests@gmail.com>
2024-01-17 13:24:00 +0100

    Added sidebar flight info

--- script/map/plane-placement.js
+++ script/map/plane-placement.js
@@ -28,24 +28,21 @@ $(document).ready(() => {
                     rotationAngle: pilot.heading,
                 });
                 let popupContent = pilot.callsign;
-                if (pilot.flight_plan) {
+                if (!pilot.flight_plan) {
+                    // if flight plan not available
+                    popupContent += pilot.altitude
+                        ? `<br>${Math.round(pilot.altitude / 50) * 50}ft`
+                        : '';
+                    popupContent += '<br>No flight plan'
+                } else {
                     popupContent += pilot.flight_plan.aircraft_short
                         ? `<br>${pilot.flight_plan.aircraft_short}`
                         : '';
                     popupContent += pilot.altitude
                         ? `<br>${Math.round(pilot.altitude / 50) * 50}ft ${pilot.groundspeed}kts tas`
                         : '';
-                    popupContent +=
-                        pilot.flight_plan.departure && pilot.flight_plan.arrival
-                            ? `<br>${pilot.flight_plan.departure} - ${pilot.flight_plan.arrival}`
-                            : '';
-                } else {
-                    // if flight plan not available
-                    popupContent += pilot.altitude
-                        ? `<br>${Math.round(pilot.altitude / 50) * 50}ft`
-                        : '';
-                    popupContent += pilot.aircraft_short
-                        ? `<br>Aircraft: ${pilot.aircraft_short}`
+                    popupContent += pilot.flight_plan.departure && pilot.flight_plan.arrival
+                        ? `<br>${pilot.flight_plan.departure} - ${pilot.flight_plan.arrival}`
                         : '';
                 }
                 marker.bindPopup(popupContent, {
@@ -58,7 +55,7 @@ $(document).ready(() => {
                 marker.on('mouseout', function () {
                     this.closePopup();
                 });
-                marker.on('click', function() {
+                marker.on('click', function () {
                     showPreciseFlightInfo(pilot);
                 });
                 markersLayer.addLayer(marker);
@@ -67,31 +64,6 @@ $(document).ready(() => {
         }
     }
 
-    function showPreciseFlightInfo(pilot) {
-        $('.f-callsign').text(pilot.callsign);
-        $('.dep-time').text((pilot.flight_plan.deptime).substring(0, 2) + ':' + (pilot.flight_plan.deptime).substring(2));
-        $('.arv-time').text((new Date(0, 0, 0, Math.floor((parseInt(pilot.flight_plan.deptime) + parseInt(pilot.flight_plan.enroute_time)) / 100), (parseInt(pilot.flight_plan.deptime) + parseInt(pilot.flight_plan.enroute_time)) % 100)).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false }));
-        $('#departureIcao').text(pilot.flight_plan.departure);
-        $('#arrivalIcao').text(pilot.flight_plan.arrival);
-        $('.flight-data-speed').text(pilot.groundspeed + ' KT');
-        $('.flight-data-altitude').text(pilot.altitude + ' FT');
-        $('.flight-data-squawk').text(pilot.transponder);
-        if ((pilot.flight_plan.flight_rules) === "I") {
-            $('.flight-data-rules').text('IFR');
-        } else if ((pilot.flight_plan.flight_rules) === "V") {
-            $('.flight-data-rules').text('VFR');
-        } else {
-            $('.flight-data-rules').text(pilot.flight_plan.flight_rules);
-        };
-        $('.flight-data-heading').text(pilot.heading + "\u00B0");
-        $('.text-details-route').text(pilot.flight_plan.route);
-        $('.text-details-remarks').text(pilot.flight_plan.remarks);
-        $('.aircraft-details-aircraft-name').text(pilot.flight_plan.aircraft_short);
-
-        $('.rectangle-parent').fadeIn( 300 );
-    }
-
-    
     function isMarkerVisible(pilot, bounds) {
         const markerLatLng = L.latLng(pilot.latitude, pilot.longitude);
         return bounds.contains(markerLatLng);
@@ -114,15 +86,15 @@ $(document).ready(() => {
         const zoom = map.getZoom();
         markersLayer.clearLayers();
         visiblePlanes = {};
-        
+
         if (L.Browser.mobile) {
 
         }
 
-        if (planesVisible) { 
+        if (planesVisible) {
             if (cachedData) {
                 let maxVisibleIcons;
-    
+
                 if (zoom <= 5) {
                     const totalPilots = cachedData.pilots.length;
                     const percentage = 0.40;
@@ -130,7 +102,7 @@ $(document).ready(() => {
                 } else {
                     maxVisibleIcons = cachedData.pilots.length;
                 }
-    
+
                 const visiblePilots = cachedData.pilots.filter((pilot) =>
                     isMarkerVisible(pilot, bounds)
                 );
@@ -157,12 +129,15 @@ $(document).ready(() => {
             }
         }
     }
+
     function checkIfPlanesStored() {
         const planesLS = localStorage.getItem('planes');
         planesVisible = planesLS !== 'hidden';
-        updatePlaneVisibility(); 
+        updatePlaneVisibility();
     }
+
     checkIfPlanesStored();
+
     function updatePlaneVisibility() {
         if (planesVisible) {
             getPlanesPosition();
@@ -172,15 +147,16 @@ $(document).ready(() => {
             clearInterval(fetchInterval);
         }
     }
+
     planeToggleBtn.click(function () {
-    planesVisible = !planesVisible;
-    if (planesVisible) {
-        localStorage.setItem('planes', 'visible');
-    } else {
-        localStorage.setItem('planes', 'hidden');
-    }
-    updatePlaneVisibility(); 
-});
+        planesVisible = !planesVisible;
+        if (planesVisible) {
+            localStorage.setItem('planes', 'visible');
+        } else {
+            localStorage.setItem('planes', 'hidden');
+        }
+        updatePlaneVisibility();
+    });
 
     map.on('moveend zoomend', function () {
         if (planesVisible) {
@@ -190,3 +166,80 @@ $(document).ready(() => {
 
     getPlanesPosition();
 });
+
+export async function showPreciseFlightInfo(pilot) {
+    $('.f-callsign').text(pilot.callsign);
+
+    if (pilot.flight_plan === null) {
+        $('.dep-time, .arv-time, .line-separator-1, .plane-icon-centered-circle, .line-separator-7, .main-icaos-div').css('display', 'none');
+        $('.rectangle-parent, .no-flight-plan-warning').fadeIn(300);
+    } else {
+        $('.no-flight-plan-warning').css('display', 'none');
+        $('.dep-time, .arv-time, .line-separator-1, .plane-icon-centered-circle, .line-separator-7, .main-icaos-div').css('display', 'flex');
+        $('.dep-time').text(`${pilot.flight_plan.deptime.substring(0, 2)}:${pilot.flight_plan.deptime.substring(2)}`);
+        let [deptimeH, deptimeM, enrouteH, enrouteM] = [parseInt(pilot.flight_plan.deptime.substring(0, 2)), parseInt(pilot.flight_plan.deptime.substring(2)), Math.floor(parseInt(pilot.flight_plan.enroute_time) / 100), parseInt(pilot.flight_plan.enroute_time) % 100];
+        let [arrivalH, arrivalM] = [(deptimeH + enrouteH + Math.floor((deptimeM + enrouteM) / 60)) % 24, (deptimeM + enrouteM) % 60];
+        $('.arv-time').text(`${arrivalH.toString().padStart(2, '0')}:${arrivalM.toString().padStart(2, '0')}`);
+    }
+
+    $('#departureIcao').text(pilot.flight_plan?.departure || 'N/A');
+    $('#arrivalIcao').text(pilot.flight_plan?.arrival || 'N/A');
+    $('.flight-data-speed').text(pilot.groundspeed + ' KT' || 'N/A');
+    $('.flight-data-altitude').text(pilot.altitude + ' FT' || 'N/A');
+    $('.flight-data-squawk').text(pilot.transponder || 'N/A');
+    $('.flight-data-altimeter-normal').text(pilot.qnh_mb + 'hPa');
+    $('.flight-data-altimeter-us').text(pilot.qnh_i_hg.toFixed(2) + 'inHg');
+    $('.aircraft-details-aircraft-name').text(pilot.flight_plan?.aircraft_short || 'N/A');
+
+    try {
+        const airlineData = await getAirlineDataFromCallsign(pilot.callsign);
+
+        if (airlineData) {
+            const logoUrl = `https://vatsim1.netlify.app/src/img/airline_logos/${airlineData.Code}_logo.png`;
+            $('.airline-logo').attr('src', logoUrl);
+            $('.f-airline').text(airlineData.Name);
+            console.log('the image and name should be in the content');
+        } else {
+            console.error("Error: Unable to find airline data for the callsign.");
+        }
+    } catch (error) {
+        console.error("Error fetching airline data:", error);
+    }
+
+    if (pilot.flight_plan) {
+        if (pilot.flight_plan.flight_rules === "I") {
+            $('.flight-data-rules').text('IFR' || 'N/A');
+        } else if (pilot.flight_plan.flight_rules === "V") {
+            $('.flight-data-rules').text('VFR' || 'N/A');
+        } else {
+            $('.flight-data-rules').text(pilot.flight_plan.flight_rules);
+        }
+    } else {
+        $('.flight-data-rules').text('N/A');
+    }
+
+    $('.flight-data-heading').text(pilot.heading + "\u00B0");
+    $('.text-details-route').text(pilot.flight_plan?.route || 'N/A');
+    $('.text-details-remarks').text(pilot.flight_plan?.remarks || 'N/A');
+    $('.aircraft-details-aircraft-name').text(pilot.flight_plan?.aircraft_short || 'N/A');
+
+    $('.rectangle-parent').fadeIn(300);
+}
+
+async function getAirlineDataFromCallsign(callsign) {
+    const firstThreeSymbols = callsign.substring(0, 3);
+    try {
+        const response = await fetch('/src/json/airlines.json');
+        const data = await response.json();
+        const matchingAirline = data.rows.find(airline => airline.ICAO === firstThreeSymbols);
+        if (matchingAirline) {
+            return matchingAirline;
+        } else {
+            console.error("Error: Airline not found in the JSON data.");
+            $('.airline-logo').attr('src', 'https://vatsim1.netlify.app/src/img/airline_logos/lo_logo.png');
+        }
+    } catch (error) {
+        console.error("Error fetching airline data:", error);
+        $('.airline-logo').attr('src', 'https://vatsim1.netlify.app/src/img/airline_logos/lo_logo.png');
+    }
+}
