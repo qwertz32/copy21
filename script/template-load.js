54bd7d179e61b9fa1749f9615a133450d69bf4ba
Oscar <szkrtests@gmail.com>
2024-01-18 23:27:30 +0100

    Template new method try

--- script/template-load.js
+++ script/template-load.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
-    $('#flight-details').load('/templates/map/plane-info.html'); //Plane info, MAP subpage
-    $('#ls-popup').load('/templates/popups/first.html');
-});
+// $(document).ready(function () {
+//     $('#flight-details').load('/templates/map/plane-info.html'); Plane info, MAP subpage
+//     $('#ls-popup').load('/templates/popups/first.html');
+// });
