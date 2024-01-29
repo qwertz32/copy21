2254c2db7ed2b8535c7f84abecca59ecb8f5492b
Oscar <szkrtests@gmail.com>
2024-01-29 16:34:06 +0100

    version now showing in the attribution

--- script/nocache-cords-and-zoom.js
+++ script/nocache-cords-and-zoom.js
@@ -84,8 +84,27 @@ $(document).ready(function () {
         keepBuffer: 5,
         zIndex: 2
     });
-    var atrCtrl = L.control.attribution({ position: 'topright' }).addTo(map);
-    atrCtrl.setPrefix('<a href="https://leafletjs.com/" target="_blank">Leaflet</a>'); //fix to remove the ukrainian flag
+
+    function currentVersionToAttribution() {
+        let attributionVersionText; 
+        $.ajax({
+            url: '/version.json',
+            dataType: 'json',
+            success: function (data) {
+                attributionVersionText = 'V' + data.version.ver + ', Build: ' + data.version.build + ' |';
+                console.log("built the attributionVerText correctly");
+                const attributionText = `${attributionVersionText} <a href="https://leafletjs.com/" target="_blank">Leaflet</a>`;
+                const atrCtrl = L.control.attribution({ position: 'topright' }).addTo(map);
+                atrCtrl.setPrefix(attributionText);
+            },
+            error: function () {
+                console.log("errored");
+                $('#loading-build-info').text('');
+            }
+        });
+    }
+    
+    currentVersionToAttribution();
 
     baseLayer.addTo(map);
 
