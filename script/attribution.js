ae342d395dfc927f46e86386d881bbd946c947fd
Oscar <szkrtests@gmail.com>
2024-02-02 18:39:51 +0100

    Fixed attribution not showing on loading screen

--- script/attribution.js
+++ script/attribution.js
@@ -2,21 +2,23 @@ $(document).ready(() => {
     function currentVersionToAttribution() {
         let attributionVersionText;
         $.ajax({
-            url: '/version.json',
-            dataType: 'json',
+            url: "/version.json",
+            dataType: "json",
             success: function (data) {
-                attributionVersionText = 'V' + data.ver + ', Build: ' + data.build + ' |';
+                attributionVersionText = "V" + data.ver + ", Build: " + data.build + " |";
                 console.log("built the attributionVerText correctly");
                 const attributionText = `${attributionVersionText} <a href="https://leafletjs.com/" target="_blank">Leaflet</a>`;
-                const atrCtrl = L.control.attribution({ position: 'topright' }).addTo(map);
+                const atrCtrl = L.control.attribution({position: "topright"}).addTo(map);
                 atrCtrl.setPrefix(attributionText);
+                loadingVersionText = attributionVersionText.slice(0, -1);
+                $("#loading-build-info").text(loadingVersionText);
             },
             error: function () {
                 console.log("errored");
-                $('#loading-build-info').text('');
-            }
+                $("#loading-build-info").text("");
+            },
         });
     }
 
     currentVersionToAttribution();
-})
+});
