34567fd8cba22ad0ef4968f45d3600605c156c58
Oscar <szkrtests@gmail.com>
2024-02-03 13:13:58 +0100

    Remove build comments

--- build/updateVersion.js
+++ build/updateVersion.js
@@ -1,10 +1,6 @@
 let fs = require('fs');
-// get current values from version
 let mainFile = 'version.json';
 let recievedData = JSON.parse(fs.readFileSync(mainFile));
-
-// add one to the build number and add current timestamp
 recievedData.build = `${parseInt(recievedData.build.split('_')[0]) + 1}_${Math.floor(Date.now() / 1000)}`;
-
-// write new values back to the version file
-fs.writeFileSync(mainFile, JSON.stringify(recievedData, null, 2));
+fs.writeFileSync(mainFile, JSON.stringify(
+    recievedData, null, 2));
