2347534b10e719f4fe779b5ecc9115b423eebb37
Oscar <szkrtests@gmail.com>
2024-02-03 13:43:26 +0100

    Now only Unix Timestamp is the build version

--- build/updateVersion.js
+++ build/updateVersion.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const file = 'version.json';
 const receivedData = JSON.parse(fs.readFileSync(file));
 const unix = Math.floor(Date.now() / 1000);
-const buildVer = unix.toString().substr(-4);
 
-receivedData.build = `${buildVer}_${unix}`;
+receivedData.build = `${unix}`;
 fs.writeFileSync(file, JSON.stringify(receivedData, null, 2));
-console.log(`Build updated to ${buildVer}_${unix}`);
+console.log(`Build updated to ${unix}`);
