19dd48d7354fe83b37f4fb3c0bb90ac33ea959e4
Oscar <szkrtests@gmail.com>
2024-02-03 13:19:07 +0100

    fix

--- build/updateVersion.js
+++ build/updateVersion.js
@@ -1,6 +1,6 @@
-let fs = require('fs');
-let mainFile = 'version.json';
-let recievedData = JSON.parse(fs.readFileSync(mainFile));
-recievedData.build = `${parseInt(recievedData.build.split('_')[0]) + 1}_${Math.floor(Date.now() / 1000)}`;
-fs.writeFileSync(mainFile, JSON.stringify(
-    recievedData, null, 2));
+const fs = require('fs');
+const file = 'version.json';
+const recievedData = JSON.parse(fs.readFileSync(file));
+const currentBuildNumber = parseInt(recievedData.build.split('_')[0]);
+recievedData.build = `${currentBuildNumber + 1}_${Math.floor(Date.now() / 1000)}`;
+fs.writeFileSync(file, JSON.stringify(recievedData, null, 2));
