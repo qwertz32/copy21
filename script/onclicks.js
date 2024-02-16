0388a1a15c2f31b77fc7e6fa835e7793cb61e59c
Oscar <szkrtests@gmail.com>
2024-02-16 19:56:53 +0100

    Mobile menu toggle addition

--- script/onclicks.js
+++ script/onclicks.js
@@ -2,4 +2,7 @@ $(document).ready(() => {
     $("#event-boxes-timeOfFetch").click(function () {
         window.location.reload();
     });
+    $(".menu-icon").click(function () {
+        $(".menu-options").fadeToggle();
+    });
 });
