06c9f3725b4ecb77d43636b9bf00cc511873276e
Oscar <szkrtests@gmail.com>
2024-02-20 21:46:00 +0100

    y

--- script/popups.js
+++ script/popups.js
@@ -1,41 +1,32 @@
-(function () {
-    let userResponse;
+let userResponse;
 
-    function shwPopUp() {
-        try {
-            const resp = localStorage.getItem("resp");
-            if (resp === "understood") {
-                return; // Do not open the popup
-            } else {
-                $("#ls-popup").fadeIn();
-            }
-        } catch (error) {
-            $("#cd-popup").fadeIn();
-            console.error("Local Storage not accessible.");
+function shwPopUp() {
+    try {
+        const resp = localStorage.getItem("resp");
+        if (resp === "understood") {
+            return; // Do not open the popup
+        } else {
+            $("#ls-popup").fadeIn();
         }
+    } catch (error) {
+        $("#cd-popup").fadeIn();
+        console.error("Local Storage not accessible.");
     }
+}
 
-    function clsPopUp() {
-        $("#ls-popup").fadeOut();
+function clsPopUp() {
+    $("#ls-popup").fadeOut();
 
-        userResponse = "understood";
-        localStorage.setItem("resp", userResponse.toString());
-    }
+    userResponse = "understood";
+    localStorage.setItem("resp", userResponse.toString());
+}
+function clsCdPopUp() {
+    $("#cd-popup").fadeOut();
+}
+$(document).ready(function () {
+    shwPopUp();
 
-    function clsCdPopUp() {
-        $("#cd-popup").fadeOut();
-    }
-
-    $(document).ready(function () {
-        shwPopUp();
-
-        $("#ls-popup-btn").click(function () {
-            clsPopUp();
-        });
+    $("#ls-popup-btn").click(function () {
+        clsPopUp();
     });
-
-    // Expose only necessary functionality
-    window.popupModule = {
-        closeCdPopUp: clsCdPopUp,
-    };
-})();
+});
